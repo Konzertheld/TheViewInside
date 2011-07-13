1fc3354a3b194c9625f350f8bcb543543c772bb6
Christian G <christian.gredig@googlemail.com>
2011-07-14 01:54:30 +0200

    Reduce footer tag cloud

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <?php if ( ! defined('HABARI_PATH' ) ) { die( _t('Please do not load this page directly.') ); } ?>
 	<div id="footer">
-		<div id="fulltagcloud" class="tag-cloud"><h2>Full Tag Cloud</h2><?php $theme->tag_cloud(); ?></div>
+		<div id="tagcloud" class="tag-cloud"><h2><?php _e("Common topics"); ?></h2><?php $theme->tag_cloud(150); ?></div>
 	</div>
 </div>
