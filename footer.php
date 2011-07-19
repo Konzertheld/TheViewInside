1aad5957f6e32552899c78d734161cecb9a4245d
Christian G <christian.gredig@googlemail.com>
2011-07-19 02:20:53 +0200

    Remove the piwik code

--- footer.php
+++ footer.php
@@ -1,27 +1,6 @@
 <?php if ( ! defined('HABARI_PATH' ) ) { die( _t('Please do not load this page directly.') ); } ?>
 	<div id="footer">
 		<div id="tagcloud" class="tag-cloud"><h2><?php _e("Common topics"); ?></h2><?php $theme->tag_cloud(150); ?></div>
+		<?php $theme->footer(); ?>
 	</div>
 </div>
-<?php 
-// -- Piwik Tracking API init -- 
-
-require_once "PiwikTracker.php";
-PiwikTracker::$URL = 'http://konzertheld.de/piwik/';
-
-$t = new PiwikTracker( $idSite = 1, 'http://konzertheld.de/piwik/');
-// Optional function calls
-$t->setResolution(1337, 42 );
-
-// set a Custom Variable called 'Gender'
-$t->setCustomVariable( 1, 'theme', $theme->name );
-if ($user instanceof User && $loggedin)
-	$t->setCustomVariable(2, 'user', $user->displayname);
-
-// Mandatory: set the URL being tracked
-$t->setUrl( $url = $_SERVER['REQUEST_URI'] );
-
-// Finally, track the page view with a Custom Page Title
-// In the standard JS API, the content of the <title> tag would be set as the page title
-$t->doTrackPageView(Options::get('title'));
-?>
