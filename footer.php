505ec9279733f2b39a52f45c134a9bf106d3fc7a
Christian G <christian.gredig@googlemail.com>
2011-08-14 04:08:07 +0200

    Made footer an area

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('HABARI_PATH' ) ) { die( _t('Please do not load this page directly.') ); } ?>
 	<div id="footer">
-		<div id="tagcloud" class="tag-cloud"><h2><?php _e("Common topics"); ?></h2><?php $theme->tag_cloud(150); ?></div>
+		<?php $theme->area('footer'); ?>
 		<?php $theme->footer(); ?>
 	</div>
 </div>
