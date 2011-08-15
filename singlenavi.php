a4b2f74eac93e1db3fc2e4b2203b3e2e4f154364
Christian G <christian.gredig@googlemail.com>
2011-08-15 02:34:28 +0200

    New pagination. Uses icons now in the title line of a post instead of text above the title.

--- singlenavi.php
+++ singlenavi.php
@@ -10,8 +10,35 @@
 	$query = " AND status={$post->status} AND ($query)";
 	$dparams = array('where' => "pubdate <= '{$post->pubdate->sql}' $query");
 	$aparams = array('where' => "pubdate >= '{$post->pubdate->sql}' $query");
+	
+	$dpost = $post->descend($dparams);
+	$apost = $post->ascend($aparams);
+	
 ?>
 
-<p class="pagination"><?php if(is_object($post->descend($dparams))):?><a href="<?php echo $post->descend($dparams)->permalink; ?>">&lt; Vorheriger Post</a> | <?php endif; ?>
-<?php if ($user instanceof User && $loggedin): ?><a href="<?php Site::out_url('admin');?>/publish?id=<?php echo $post->id; ?>" title="<?php printf(_t('Edit %s'), $post->title); ?>"><?php _e('Edit post', $theme->name); ?></a><?php endif;?><? if ($user instanceof User && $loggedin)echo ' | ';?>
-<?php if(is_object($post->ascend($aparams))):?> <a href="<?php echo $post->ascend($aparams)->permalink; ?>">N&auml;chster Post &gt;</a><?php endif; ?></p>
+<p class="pagination">
+<?php if(is_object($dpost)): ?>
+	<a href="<?php echo $dpost->permalink; ?>">
+	<img src="<?php Site::out_url('theme'); ?>/images/icons/arrow_left.png" alt="<?php _e('Previous Post', $theme->name); ?>" title="<?php _e('Previous Post', $theme->name); ?>" class="paginationicon">
+	</a>
+<?php else: ?>
+	<img src="<?php Site::out_url('theme'); ?>/images/icons/arrow_left_g.png" alt="<?php _e('There is no previous Post', $theme->name); ?>" title="<?php _e('There is no previous Post', $theme->name); ?>" class="paginationicon">
+<?php endif; ?>
+<a href="<?php Site::out_url('habari'); ?>">
+<img src="<?php Site::out_url('theme'); ?>/images/icons/arrow_up.png" alt="<?php _e('Back to homepage', $theme->name); ?>" title="<?php _e('Back to homepage', $theme->name); ?>" class="paginationicon">
+</a>
+<?php if ($user instanceof User && $loggedin): ?>
+	<a href="<?php Site::out_url('admin');?>/publish?id=<?php echo $post->id; ?>">
+	<img src="<?php Site::out_url('theme'); ?>/images/icons/page_edit.png" alt="<?php _e('Edit post', $theme->name); ?>" title="<?php _e('Edit post', $theme->name); ?>" class="paginationicon">
+	</a>
+<?php endif;?>
+<a href="<?php echo $post->permalink;?>/atom/comments">
+<img src="<?php Site::out_url('theme'); ?>/images/icons/feed.png" alt="<?php _e('Subscribe to this post\'s comments', $theme->name); ?>" title="<?php _e('Subscribe to this post\'s comments', $theme->name); ?>" class="paginationicon">
+</a>
+<?php if(is_object($apost)): ?>
+	<a href="<?php echo $apost->permalink; ?>">
+	<img src="<?php Site::out_url('theme'); ?>/images/icons/arrow_right.png" alt="<?php _e('Next Post', $theme->name); ?>" title="<?php _e('Next Post', $theme->name); ?>" class="paginationicon">
+	</a>
+<?php else: ?>
+	<img src="<?php Site::out_url('theme'); ?>/images/icons/arrow_right_g.png" alt="<?php _e('There is no next Post', $theme->name); ?>" title="<?php _e('There is no next Post', $theme->name); ?>" class="paginationicon">
+<?php endif; ?></p>
