37784c413bd7c1d49036e93e9ab3e72b55b1e8ce
Christian G <christian.gredig@googlemail.com>
2011-08-15 06:47:12 +0200

    Show the last modification date in the metaline for entries. Pages have it already and events don't even show the date of creation.

--- metaline.entry.php
+++ metaline.entry.php
@@ -1,2 +1,6 @@
-<?php echo sprintf(_t("An article from %s", $theme->name), $theme->post->pubdate->format()); ?>
-<?php if($theme->post->isguestpost) printf(" - "._t('Guest article by %s', $theme->name), $theme->post->author->displayname); ?>
+<?php
+if($theme->post->pubdate == $theme->post->updated)
+	echo sprintf(_t("An article from %s", $theme->name), $theme->post->pubdate->format());
+else
+	echo sprintf(_t("An article from %s, last updated %s", $theme->name), $theme->post->pubdate->format(), $theme->post->updated->format());
+if($theme->post->isguestpost) printf(" - "._t('Guest article by %s', $theme->name), $theme->post->author->displayname); ?>
