2fd8307f1496f12c7eca61cad880a1f558954bac
Christian G <christian.gredig@googlemail.com>
2011-08-15 07:02:29 +0200

    New feature: Added some icons to posts in multiple views, similar to the new pagination in single view - jump to top, jump to comments and edit post.

---
+++ multinavi.php
@@ -0,0 +1,13 @@
+<p class="postnavi">
+<a href="<?php echo $content->permalink; ?>#comments">
+<img src="<?php Site::out_url('theme'); ?>/images/icons/comments.png" alt="<?php _e('Jump to comments', $theme->name); ?>" title="<?php _e('Jump to comments', $theme->name); ?>" class="paginationicon">
+</a>
+<?php if ($user instanceof User && $loggedin): ?>
+	<a href="<?php Site::out_url('admin');?>/publish?id=<?php echo $content->id; ?>">
+	<img src="<?php Site::out_url('theme'); ?>/images/icons/page_edit.png" alt="<?php _e('Edit post', $theme->name); ?>" title="<?php _e('Edit post', $theme->name); ?>" class="paginationicon">
+	</a>
+<?php endif;?>
+<a href="#">
+<img src="<?php Site::out_url('theme'); ?>/images/icons/arrow_up.png" alt="<?php _e('Jump to top', $theme->name); ?>" title="<?php _e('Jump to top', $theme->name); ?>" class="paginationicon">
+</a>
+</p>
--- post.php
+++ post.php
@@ -1,6 +1,7 @@
 <?php if ( ! defined('HABARI_PATH' ) ) { die( _t('Please do not load this page directly.') ); } ?>
 <div class="multiple post <?php echo Post::type_name($content->content_type[0]); ?><?php if($theme->evenodd) echo ' even'; else echo ' odd';?>">
 	<div class="postmeta">
+		<?php include("multinavi.php"); ?>
 		<h2 class="postmeta-title"><a href="<?php echo $content->permalink; ?>" title="<?php echo $content->title_out; ?>"><?php echo $content->title_out; ?></a></h2>
 		<div class="postmeta-meta">
 			<?php $theme->metaline($content); ?>
