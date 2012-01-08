a57a60b03fc46fd1037a6ccb3c7c7574c71abc7e
Konzertheld <christian.gredig@googlemail.com>
2012-01-08 21:10:02 +0100

    Ass a hook for the multi jumplist.

--- multinavi.php
+++ multinavi.php
@@ -11,4 +11,5 @@
 <a href="#">
 <img src="<?php Site::out_url('theme'); ?>/images/icons/arrow_up.png" alt="<?php _e('Jump to top', $theme->name); ?>" title="<?php _e('Jump to top', $theme->name); ?>" class="paginationicon">
 </a>
+<?php echo $theme->jumplist($content, true); ?>
 </p>
