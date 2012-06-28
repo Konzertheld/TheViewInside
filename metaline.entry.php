cb2b827f2278710af1e8b58e0428e16d0df6d78f
Konzertheld <christian.gredig@googlemail.com>
2012-06-28 22:58:09 +0200

    Don't show the metaline for entries without tags.

--- metaline.entry.php
+++ metaline.entry.php
@@ -1,6 +1,9 @@
 <?php
-if($theme->post->pubdate == $theme->post->updated)
-	echo sprintf(_t("An article from %s", $theme->name), $theme->post->pubdate->format());
-else
-	echo sprintf(_t("An article from %s, last updated %s", $theme->name), $theme->post->pubdate->format(), $theme->post->updated->format());
-if($theme->post->isguestpost) printf(" - "._t('Guest article by %s', $theme->name), $theme->post->author->displayname); ?>
+if(count($theme->post->tags))
+{
+	if($theme->post->pubdate == $theme->post->updated)
+		echo sprintf(_t("An article from %s", $theme->name), $theme->post->pubdate->format());
+	else
+		echo sprintf(_t("An article from %s, last updated %s", $theme->name), $theme->post->pubdate->format(), $theme->post->updated->format());
+	if($theme->post->isguestpost) printf(" - "._t('Guest article by %s', $theme->name), $theme->post->author->displayname);
+}	?>
