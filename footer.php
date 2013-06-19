4acaa62f2d8faf3817a2983b23b308a955f1bbfc
Konzertheld <github@konzertheld.de>
2013-06-19 17:37:15 +0200

    Remove another rest of Google Plus

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
 <?php if ( ! defined('HABARI_PATH' ) ) { die( _t('Please do not load this page directly.') ); } ?>
 	<div id="footer">
 		<?php echo $theme->area('footer'); ?>
-		<?php if(($multipleview && $gpmultiple) || (!$multipleview && $gpsingle)):?><script type="text/javascript">gapi.plusone.go();</script><?php endif; ?>
 		<?php echo $theme->footer(); ?>
 	</div>
 </div>
