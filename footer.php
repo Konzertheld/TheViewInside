09340da0e6eaeac23509dd180970dc52daf82076
Konzertheld <christian.gredig@gmail.com>
2013-07-31 01:30:46 +0200

    Put footer into container and colorize it.

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
 <?php if ( ! defined('HABARI_PATH' ) ) { die( _t('Please do not load this page directly.') ); } ?>
-	<div id="footer">
-		<?php echo $theme->area('footer'); ?>
-		<?php echo $theme->footer(); ?>
+	<div id="footercontainer">
+		<div id="footer">
+			<?php echo $theme->area('footer'); ?>
+			<?php echo $theme->footer(); ?>
+		</div>
 	</div>
 </div>
