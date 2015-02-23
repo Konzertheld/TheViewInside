09e0af35f9fcc73d8eaae3151e40c86cf9610cef
Konzertheld <github@konzertheld.de>
2015-02-23 01:51:45 +0100

    Fix dl bottom margin, use colored background gradient in a nicer way, fix footer CSS

--- footer.php
+++ footer.php
@@ -1,11 +1,9 @@
 <?php if ( ! defined('HABARI_PATH' ) ) { die( _t('Please do not load this page directly.') ); } ?>
 	<div id="footercontainer">
 		<div id="footerbarcontainer">
-			<div id="footerbarcenter">
-				<div id="footerbar">
-					<?php echo $theme->area('footerbar'); ?>
-					<?php echo $theme->footer(); ?>
-				</div>
+			<div id="footerbar">
+				<?php echo $theme->area('footerbar'); ?>
+				<?php echo $theme->footer(); ?>
 			</div>
 		</div>
 	</div>
