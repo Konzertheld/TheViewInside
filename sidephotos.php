306b3c680186a93e5cafb1c0cad880aa3fb41573
Konzertheld <github@konzertheld.de>
2015-04-04 23:46:42 +0200

    Link sidephotos generated from Silo to gallery

--- sidephotos.php
+++ sidephotos.php
@@ -2,7 +2,7 @@
 foreach($content->tvi_photos as $photo)
 {
 	if($photo instanceof MediaAsset) {
-		?><a href="<?php echo $photo->url; ?>"><img src="<?php echo $photo->thumbnail; ?>" alt="<?php echo $photo->title; ?>"></a><?php
+		?><a href="<?php echo Site::get_url('site'); ?>gallery/<?php echo $content->slug; ?>?start=<?php echo $photo->path; ?>"><img src="<?php echo $photo->thumbnail; ?>" alt="<?php echo $photo->title; ?>"></a><?php
 	}
 	else {
 		echo $photo;
