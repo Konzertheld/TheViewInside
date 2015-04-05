2c4545423289beddd6b2ceddd5dc42a0b53fe56d
Konzertheld <github@konzertheld.de>
2015-04-05 02:56:31 +0200

    Significantly improve display on mobile devices using responsive CSS.

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 	<title><?php if(!$multipleview) { echo $post->title." | "; Options::out( 'title' ); } else { Options::out( 'title' ); echo ' | '; Options::out( 'tagline' ); } ?></title>
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 	<meta name="generator" content="Habari <?php echo Version::get_habariversion() ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="<?php Site::out_url( 'theme' ); ?>/style.css" media="screen" />
 	<link rel="stylesheet" type="text/css" href="<?php Site::out_url( 'theme' ); ?>/color.css" media="screen" />
 	<link rel="stylesheet" type="text/css" href="<?php Site::out_url( 'theme' ); ?>/user.css" media="screen" />
@@ -17,16 +18,16 @@
 <body>
 <div id="wrapper"><div id="innerwrapper">
 	<div id="headercontainer">
+		<h1 id="sitetitle"><a href="<?php Site::out_url('habari'); ?>" title="<?php _t('Home'); ?>"><?php Options::out( 'title' ); ?></a></h1>
+		<a id="tagline" href="<?php Site::out_url('habari'); ?>" title="<?php _t('Home'); ?>"><?php Options::out( 'tagline' ); ?></a>
+		<div id="socialcontainer">
+			<?php echo $theme->socialneticons(); ?>
+		</div>
 		<div id="headerbarcontainer">
 			<div id="headerbarcenter">
 				<div id="headerbar">
 					<?php echo $theme->area('headerbar'); ?>
 				</div>
 			</div>
-		</div>
-		<h1 id="sitetitle"><a href="<?php Site::out_url('habari'); ?>" title="<?php _t('Home'); ?>"><?php Options::out( 'title' ); ?></a></h1>
-		<a id="tagline" href="<?php Site::out_url('habari'); ?>" title="<?php _t('Home'); ?>"><?php Options::out( 'tagline' ); ?></a>
-		<div id="socialcontainer">
-			<?php echo $theme->socialneticons(); ?>
 		</div>
 	</div>
